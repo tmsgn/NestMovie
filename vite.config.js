e3ca6ff447aa2cf64c0fa9e21914e4777406b4fd
Temesgen <tmsgn1996@gmail.com>
2025-03-17 12:58:52 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'development' ? '/NestMovie/' : '/',
-    server: {
-      host: '0.0.0.0',
-      port: 3000,
-    },
-  };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/NestMovie/',
+  server: {
+    host: '0.0.0.0', 
+    port: 3000,
+  },
+})
