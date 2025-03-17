8e06d13fdfea74edceb75b8e0822ad177bda2009
Temesgen <tmsgn1996@gmail.com>
2025-03-17 11:25:46 +0300

    Updated Video.jsx with new features and fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,19 @@ import NavBar from '../components/NavBar';
 import { Context } from '../context/Context';  
 import MovieCard from '../components/MovieCard';  
 import Footer from '../components/Footer';
+
 const Home = () => {  
   const { movies, tvShows, loading, error } = useContext(Context);  
 
   if (loading) {  
-    return  <div className='flex space-x-2 justify-center items-center bg-[rgba(0, 0, 0, 0.897)] h-screen dark:invert'>
-    <span class='sr-only'>Loading...</span>
-     <div class='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-   <div class='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-   <div class='h-8 w-8 bg-white rounded-full animate-bounce'></div>
- </div>
+    return (
+      <div className='flex space-x-2 justify-center items-center bg-[rgba(0, 0, 0, 0.897)] h-screen dark:invert'>
+        <span className='sr-only'>Loading...</span>
+        <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+        <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+        <div className='h-8 w-8 bg-white rounded-full animate-bounce'></div>
+      </div>
+    );
   }  
 
   if (error) {  
@@ -24,17 +27,17 @@ const Home = () => {
     <div className='w-full overflow-x-hidden'>  
       <NavBar />  
       <div className=''>
-      <ImgSlider />  
-      <div className='text-white p-5'>  
-        <h1 className='text-3xl font-bold mb-4'>Trending Movies</h1>  
-        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3'>  
-          <MovieCard movies={movies} mediaType="movie"/>  
+        <ImgSlider />  
+        <div className='text-white p-5'>  
+          <h1 className='text-3xl font-bold mb-4'>Trending Movies</h1>  
+          <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3'>  
+            <MovieCard movies={movies} mediaType="movie"/>  
+          </div>  
+          <h1 className='text-3xl font-bold mb-4 mt-10 '>Trending TV Shows</h1>  
+          <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3'>  
+            <MovieCard movies={tvShows} mediaType="tv"/>  
+          </div>   
         </div>  
-        <h1 className='text-3xl font-bold mb-4 mt-10 '>Trending TV Shows</h1>  
-        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-3'>  
-          <MovieCard movies={tvShows} mediaType="tv"/>  
-        </div>   
-      </div>  
       </div>
       <Footer />
     </div>  
