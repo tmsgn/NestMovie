3db7966963c648fd9ddd9dea5f7fd9ed466bebd5
Temesgen <tmsgn1996@gmail.com>
2025-03-30 12:58:14 +0300

    Update configuration for custom domain and client-side routing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://nestmovie.live/',
+  base: process.env.NODE_ENV === 'production' ? 'https://nestmovie.live/' : '/',
   server: {
     host: '0.0.0.0', 
     port: 3000,
